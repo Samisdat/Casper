356ec054e5aec39f4845e75c93888aba651b5a5a
samisdat <bastian.sackermann@gmail.com>
2015-09-06 18:40:58 +0000

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/*global module, require, process */
+
+module.exports = function(grunt) {
+    "use strict";
+
+    require('load-grunt-tasks')(grunt);
+    require('load-grunt-config')(grunt);
+
+};
