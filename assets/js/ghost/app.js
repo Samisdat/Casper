45d6a9994cb64cacf8d249e217af6034fc50876e
samisdat <bastian.sackermann@gmail.com>
2015-09-20 20:46:38 +0000

    no mor faux column in js

--- assets/js/ghost/app.js
+++ assets/js/ghost/app.js
@@ -4,28 +4,10 @@
 
     "use strict";
 
-    //productive
-
-    Samisdat.FauxColumns = (function(){
-       var $aside;
-       var height = function(){
-           var height = $aside.parents('.row').height();
-           $aside.height(height); 
-       };
-       var ready = function(){
-           $aside = $('aside.col-md-4');
-           if($aside.get(0) === undefined){
-               return;
-           }
-           height();  
-       };
-       $(document).ready(ready);
-    }());
-
 	Samisdat.PositionConway = ( function() {
 		var $conway;
 		var invisible = false;
-		
+
 		var scroll = function(){
 			if(invisible === false && $(window).scrollTop() > 250){
 				invisible = true;
@@ -39,7 +21,7 @@
 			}
 		};
 		var ready = function() {
-			$conway = $('#conway'); 
+			$conway = $('#conway');
 			$(window).scroll(scroll);
 			scroll();
 		};
@@ -47,7 +29,4 @@
 		$(document).ready(ready);
 	}());
 
-
-    //\productive    
-    
 })(jQuery);
